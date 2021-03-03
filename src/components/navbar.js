34484a606902ffea7f33fc530feede411b7c0f53
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-03-03 21:41:32 +0100

    feat: :sparkles: Added navbar
    Replaced placeholder with actual navbar-like component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,17 @@
+import "../styles/navbar.css";
+
 const Navbar = () => {
-  return <div className="navbar"></div>;
+  return (
+    <div className="navbar">
+      <div className="content">
+        <div className="brand item">
+          <a href="/" className="">
+            Covid Counter
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
